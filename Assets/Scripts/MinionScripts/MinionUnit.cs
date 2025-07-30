fc01a211387fa3ac275d98d4f3dc4a1e83e90d12
Xabier <86005264+xaju5@users.noreply.github.com>
2025-07-30 19:03:19 +0200

    build fixes

--- Assets/Scripts/MinionScripts/MinionUnit.cs
+++ Assets/Scripts/MinionScripts/MinionUnit.cs
@@ -1,10 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.UIElements;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MinionUnit : MonoBehaviour
 {
